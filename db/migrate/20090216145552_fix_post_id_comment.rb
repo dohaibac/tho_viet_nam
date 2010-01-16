0203854b6e089493d4768c82e89fd08af11cce80
Do Hai Bac <dohaibac@gmail.com>
2010-01-16 16:44:37 +0700

    delete everything, change to php language

--- db/migrate/20090216145552_fix_post_id_comment.rb
+++
@@ -1,9 +0,0 @@
-class FixPostIdComment < ActiveRecord::Migration
-  def self.up
-    rename_column :comments, :posi_id, :post_id
-  end
-
-  def self.down
-    rename_column :comments, :post_id, :posi_id
-  end
-end
