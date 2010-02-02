b9c6904851245ff6567a29b2d3d75beaafbec20f
dohaibac <dohaibac#gmail.com>
2010-02-02 18:40:57 +0700

    make form author input

---
+++ system/application/controllers/admin/admin_author.php
@@ -0,0 +1,41 @@
+<?php
+
+class Admin_author extends Controller {
+
+    //public $my_data = array();
+
+	function Admin_author(){
+    
+		parent::Controller();
+        $this -> load -> model("author_model");
+        //$this->load->library('MyMenu');
+	}
+	
+	function index(){
+        
+        $data['query'] = $this -> author_model -> all();
+        $data['title'] = "author";
+		$this->load->view('/author/all_author', $data);
+	}
+    
+    function get($id=null){
+    
+        if (!$id){
+            $id = 1;
+        }
+        
+        $data['title'] = "author";
+        $data['query'] = $this -> author_model -> get_author($id);
+        $this->load->view('/author/show', $data);
+    }
+    
+    function input(){
+        $this->load->helper('form');
+        $this->load->model('author_model');
+        $data = $this -> author_model -> general();
+        $this -> load -> view('admin/author/new',$data);
+    }
+
+}
+
+?>
--- system/application/models/author_model.php
+++ system/application/models/author_model.php
@@ -18,17 +18,12 @@
         
         function general(){
         
-            $data['base']       = $this->config->item('base_url');
+            $data['title'] = 'Add new author';
+            $data['base']  = $this->config->item('base_url');
             
-            $data['title']      = 'Title';
-            $data['author']     = 'Author';
-            $data['publisher']  = 'Publisher';
-            $data['year']       = 'Year';
-            $data['years']      = array('2007'=>'2007',
-                                        '2008'=>'2008',
-                                        '2009'=>'2009');
-            $data['available']  = 'Available';
-            $data['summary']    = 'Summary';
+            $data['name']  = 'name';
+            $data['introduction']     = 'introduction';
+            $data['photo_file_name']  = 'photo_file_name';
 
             return $data;
         }
